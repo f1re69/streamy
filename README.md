824dd82ded75dd04489b82178ceffec2a2d0467a
Khelil Bezzouh <kbezzouh@gmail.com>
2020-04-03 12:44:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Project Title
+
+RESTful webservice which allow users to manage and read video streams.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+You need to create OAuth2 credential. Check the official documentation here : https://developers.google.com/adwords/api/docs/guides/authentication?hl=fr
+
+One you get your OAuth2 credential, do the following instructions.
+
+```
+cd clients
+echo "REACT_APP_GAUTH_ID=YOURCREDENTIAL" > .env
+```
+
+Once you create the .env file you can run the backend server and the frontend server with npm start.
+
+## Built With
+
+* [React.js] (https://reactjs.org) - The frontend library used
+* [Redux] (https://redux.js.org/) - State manager library 
+* [JSON Server] (https://www.npmjs.com/package/json-server) - The backend REST server
+* [Semantic UI] (https://semantic-ui.com/) - CSS Framework
+
+## Authors
+
+* **Khelil Bezzouh** - *Initial work* - [kbezzouh](https://github.com/f1re69)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ client/src/components/streams/StreamEdit.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {connect} from 'react-redux'
+import _ from 'lodash'
+import { fetchStream , editStream } from '../../actions'
+import StreamForm from './StreamForm'
+
+/* StreamEdit is an extension of the component StreamForm */
+class StreamEdit extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id)
+    }
+
+    onSubmit = (formValues) => {
+        this.props.editStream(this.props.match.params.id, formValues)
+    }
+
+    render() { 
+        console.log(this.props)
+        if (!this.props.stream) {
+            return <div>Waiting...</div>
+        }
+        return(
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm 
+                    // lodash.pick() use only the field "title" and "description" as initial field value 
+                    initialValues={_.pick(this.props.stream, 'title', 'description')} 
+                    onSubmit={this.onSubmit} 
+                />
+            </div>
+        )
+    }   
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return {stream: state.streams[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit)
